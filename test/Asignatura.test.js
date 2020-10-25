601598eec240968b8a466bc336e29e40089cf828
ablazleon <ablazleon@gmail.com>
2020-10-25 16:46:23 +0100

    Tests to access the subject

--- test/Asignatura.test.js
+++ test/Asignatura.test.js
@@ -10,11 +10,29 @@ contract('Asignatura', (accounts)=> {
         this.asignatura = await Asignatura.deployed()
     })
 
-    //Deben existir métodos para poder consultar el nombre y el curso academico de la asignatura creada.
-//Y también para acceder a la dirección del profesor.
+    //
+//
     describe('Despliegue:', async () => {
         it('Se despliega: Crear un contrato inteligente llamado Asignatura para representar una asignatura de la carrera.', async () => {
             const address = await this.asignatura.address
+            assert.notEqual(address, 0x0)
+            assert.notEqual(address, '')
+            assert.notEqual(address, null)
+            assert.notEqual(address, undefined)
+        })
+
+        it('Deben existir métodos para poder consultar el nombre', async () => {
+            const name = await this.asignatura.nombreAsignatura()
+            assert.equal(name, 'BCDA')
+        })
+
+        it('y el curso academico de la asignatura creada', async () => {
+            const ca = await this.asignatura.cursoAcademico()
+            assert.equal(ca, '2020')
+        })
+
+        it('Y también para acceder a la dirección del profesor.', async () => {
+            const address = await this.asignatura.direccionProfesor()
             assert.notEqual(address, 0x0)
             assert.notEqual(address, '')
             assert.notEqual(address, null)
